a1606ca81de92c0952f1cac791c96b9089c36b0c
madlp24 <120478064+madlp24@users.noreply.github.com>
2025-10-01 16:09:37 -0500

    Load static assets in base.html

--- events/urls.py
+++ events/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.event_list, name="event_list"),
-    path("<int:pk>/", views.event_detail, name="event_detail"),
-
-    # Organizer CRUD
+    path("events/", views.event_list, name="event_list"),
+    path("events/<int:pk>/", views.event_detail, name="event_detail"),
+    # Organizer
+    path("events/new/", views.event_create, name="event_create"),
+    path("events/<int:pk>/edit/", views.event_update, name="event_update"),
     path("venues/new/", views.venue_create, name="venue_create"),
-    path("new/", views.event_create, name="event_create"),
-    path("<int:pk>/edit/", views.event_update, name="event_update"),
 ]
+
