89c8db10afacfcb25d5c751773a61ba75b689238
madlp24 <120478064+madlp24@users.noreply.github.com>
2025-10-03 16:52:48 -0500

    SEO slugs, organizer dasdhboard and CSV export

--- events/urls.py
+++ events/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("events/", views.event_list, name="event_list"),
-    path("events/<int:pk>/", views.event_detail, name="event_detail"),
-    # Organizer
-    path("events/new/", views.event_create, name="event_create"),
-    path("events/<int:pk>/edit/", views.event_update, name="event_update"),
-    path("venues/new/", views.venue_create, name="venue_create"),
+    path("", views.event_list, name="event_list"),
+    path("<int:pk>/", views.event_detail, name="event_detail"),           
+    path("s/<slug:slug>/", views.event_detail_slug, name="event_detail_slug"),  
+    # organizer
+    path("dashboard/", views.organizer_dashboard, name="organizer_dashboard"),
+    path("export.csv", views.export_events_csv, name="export_events_csv"),
+    path("new/", views.event_create, name="event_create"),
+    path("<int:pk>/edit/", views.event_update, name="event_update"),
+    path("export.csv", views.export_events_csv, name="export_events_csv"),
+
 ]
 
