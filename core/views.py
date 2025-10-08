e4a2aab11a1299a296c638fa5cf82746ec83fb21
madlp24 <120478064+madlp24@users.noreply.github.com>
2025-10-08 09:32:47 -0500

    Changed some style of the home page

--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
+# core/views.py
 from django.shortcuts import render
+from events.models import Event
 
-# Create your views here.
 def home(request):
-    return render(request, "core/home.html")
+    events = (
+        Event.objects.filter(published=True)
+        .order_by("starts_at")
+        .select_related("venue")[:6]
+    )
+    return render(request, "core/home.html", {"events": events})
 
 def about(request):
     return render(request, "core/about.html")
